d4ee2f0c2571f9571276a2e21fa25165d55265bd
waldo2018 <wanghaichao2012@w.noreply.github.com>
2020-07-04 20:37:45 +0900

    basedao

--- src/com/java/test/dao/BaseDao.java
+++ src/com/java/test/dao/BaseDao.java
@@ -1,5 +1,12 @@
 package com.java.test.dao;
 
-public class BaseDao {
+import org.apache.commons.dbutils.QueryRunner;
 
+public abstract class BaseDao<T> {
+	
+	private QueryRunner queryRunner = new QueryRunner();
+	
+	// 定义一个变量来接受泛型的类型
+	private Class<T> type;
+ 
 }
